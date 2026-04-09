d5091939d78e84d342473789f4b9f2554d4693c6
Дмитрий Цитман <dimazzik@gmail.com>
2026-04-09 18:52:39 +0300

    lab6: fixes

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -7,12 +7,14 @@ public class Movie
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "Без наименования нельзя!")]
     public string? Title { get; set; }
 
     [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
 
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(30)]
     public string? Genre { get; set; }
 
     
